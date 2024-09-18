c592d628cfe70310e490c8300c61c4659d8117ae
SP-maXX <maxspeeder12@gmail.com>
2024-09-18 13:26:37 +0530

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -1,1 +1,25 @@
+const {cmd , commands} = require('../command')
+const fg = require('api-dylux')
+const yts = require('yt-search')
+
+
+cmd({
+    pattern: "song",
+    desc: "download songs",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("*Please give me a url or title...*")
+const _search = await yts(q)
+const data = search.videos[0]
+const url = data.url
+
+let desc =
+  
+}catch(e){
+  console.log(e)
+  reply('${e}')
+})
 
