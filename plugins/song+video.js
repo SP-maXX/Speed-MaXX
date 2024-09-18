bbd48a834d5c4c24b752d4ef8d7da1784508751d
SP-maXX <maxspeeder12@gmail.com>
2024-09-18 21:01:25 +0530

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -16,8 +16,26 @@ const _search = await yts(q)
 const data = search.videos[0]
 const url = data.url
 
-let desc =
-  
+let desc = `
+▢───────────────────────────▢
+Ӂ ─── Ｓᴘᴇᴇᴅ ＭᴀＸＸ sᴏɴɢ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ 🔊
+▢───────────────────────────▢
+» `ᴛɪᴛʟᴇ` : ${data.title}
+» `ᴀʙᴏᴜᴛ` : ${data.description.}
+» `ᴅᴜʀᴀᴛɪᴏɴ` : ${data.timestamp}
+» `ᴀɢᴏ` : ${data.ago}
+» `ᴠɪᴇᴡꜱ` : ${data.views}
+
+`ᴳᵉⁿᵉʳᵃᵗᵉᵈ ᴮʸ ©Ｓᴘᴇᴇᴅ ＭᴀＸＸ- ＭＤ₂ₒ₂₄`
+`
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+//download audio
+
+let down = await fg.yta(url)
+let downloadUrl = down.dl_url
+
+//send audio massege
+await conn.sendMassege(from,{audio: {url:downloadurl},mimtype:audio/mpeg"},{quoted:mek})
 }catch(e){
   console.log(e)
   reply('${e}')
