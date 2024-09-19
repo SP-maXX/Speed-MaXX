3434edbe36fcb918685667c27dbcbf875729bfb2
SP-maXX <maxspeeder12@gmail.com>
2024-09-19 11:35:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "I2VXyY6D#frWSU3SOkbqJ5BIRDLDCWJsfpvzytLd7xxUmMRefmCs",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/10pBNvp/Speed-Maxx.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "╭───────────────────────▢
 
