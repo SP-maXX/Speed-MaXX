124c0ecf3795d6d52ba6e4ddb47eab3cb51aa5b6
SP-maXX <maxspeeder12@gmail.com>
2024-09-30 09:11:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "I2VXyY6D#frWSU3SOkbqJ5BIRDLDCWJsfpvzytLd7xxUmMRefmCs",
+SESSION_ID: process.env.SESSION_ID || "VrcDRSgY#grw-A5Auy2WdlM5SdlIBrSzQIto0xTULkk167rCEJCY",
 MONGODB: process.env.MONGODB || "mongodb://mongo:NgEkrWjymgxUMhUOuYARMrXYQILIALLx@junction.proxy.rlwy.net:30809",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/10pBNvp/Speed-Maxx.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "╭───────────────────────▢\n│─ Hellow Boss, I am Online Now! 🌚👋\n│─ *`ʙᴏᴛ`* : Ｓᴘᴇᴇᴅ ＭᴀＸＸ- ＭＤ\n│─ *`ᴠᴇʀsɪᴏɴ`* : 1.0.0\n│─ *`ᴏᴡɴᴇʀ`* : Ｓᴀɴᴛʜᴜsʜ.Ｓ\n│─ *`ɴᴜᴍʙᴇʀ`* : 94772726437\n│─ *`ʙᴀsᴇᴅ ᴏɴ`* : Ｇɪᴛʜᴜʙ\n│─ *`ᴡᴏʀᴋ ᴛʏᴘᴇ`* : Ｐᴜʙʟɪᴄ\n│─ *`ᴘʀᴇғɪx`* : [ . ]\n╰───────────────────────▢",
